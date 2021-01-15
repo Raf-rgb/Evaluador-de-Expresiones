1354b6553651a41a76c06a04cedfcd85444c370d
Rafael <59372766+Raf-rgb@users.noreply.github.com>
2021-01-15 11:03:15 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Evaluador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable que guarda el comando
+            // que ingrese el usuario.
+            string input;
+
+            // Se ejecuta un ciclo while hasta
+            // que el usuario ejecuta el comando
+            // 'exit'
+            while(true) {
+                Console.Write("\n  > ");
+                
+                // Se guarda la expresion o comando
+                // que el usuario ingrese.
+                input = Console.ReadLine();
+
+                // Si es el comando exit, se termina
+                // el comando.
+                if(input.Equals("exit")) break;
+                // Si es el comando cls, se limpia
+                // la consola.
+                else if(input.Equals("cls")) Console.Clear();
+                // Si no es un comando, entonces se resuelve
+                // la expresion matematica.
+                else Console.WriteLine("  - " + Eval.Solve(input));
+            }
+        }
+    }
+}
